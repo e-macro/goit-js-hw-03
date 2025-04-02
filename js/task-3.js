a6461569ea25e131c4eeb923f32d2018a1a7eda8
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-04-02 19:27:48 +0300

    .

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     let array = [];
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
             array.push(numbers[i]);
         }
